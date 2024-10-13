78c122e607942b6c5d294727d1ef7f1c61a0acaa
Md Muzammil Imam <158178899+MDMUZAMMIL-19@users.noreply.github.com>
2024-10-13 16:50:08 +0530

    Update switchToLED_nexyss4DDR_constraints.v

--- SwitchToLED/switchToLED_nexyss4DDR_constraints.v
+++ SwitchToLED/switchToLED_nexyss4DDR_constraints.v
@@ -3,3 +3,5 @@ set_property IOSTANDARD LVCMOS33 [get_ports slideSwitch]
 
 set_property PACKAGE_PIN H17 [get_ports led]
 set_property IOSTANDARD LVCMOS33 [get_ports led]
+
+In Vivado this file is saved as .xdc file.
